a5a5164e13b571b216d3237acce6e32d65b2e970
danniedanniedannie <danielle.simone.lynch@gmail.com>
2019-07-05 17:28:59 +0000

    adding destroy action to the controller and destroy action test in rspec file

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+ describe "grams#destroy action" do
+it "should allow a user to destroy grams" do
+  gram = FactoryBot.create(:gram)
+  delete :destroy, params: { id: gram.id }
+  expect(response).to redirect_to root_path
+  gram = Gram.find_by_id(gram.id)
+  expect(gram).to eq nil
+end
+ 
+it "should return a 404 message if we cannot find a gram with the id that is specified" do
+  delete :destroy, params: { id: 'SPACEDUCK' }
+  expect(response).to have_http_status(:not_found)
+  end
+end 
+
+
+
 
 describe "grams#update action" do 
   it "should allow users to succesfully upadte grams" do 
@@ -9,8 +26,7 @@ describe "grams#update action" do
   expect(response).to redirect_to root_path
   gram.reload
   expect(gram.message).to eq "changed"
-
- end 
+end 
  
  it "should have 404 http error if the gram is not found" do 
  gram = FactoryBot.create(:gram, message: "Initial Value")
